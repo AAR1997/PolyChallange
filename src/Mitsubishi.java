269378562cf11da52b3e8b16149b9f6cd625e0b2
AAR1997 <95615977+AAR1997@users.noreply.github.com>
2024-04-29 15:08:24 -0400

    Initial commit

---
+++ src/Mitsubishi.java
@@ -0,0 +1,19 @@
+public class Mitsubishi extends Car {
+
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    public String startEngine() {
+        return super.startEngine();
+    }
+
+    public String accelerate() {
+        return super.accelerate();
+    }
+
+    @Override
+    public String brake() {
+        return super.brake();
+    }
+}
